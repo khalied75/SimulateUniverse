fdf7c516d9016eb6c8da08906f871ace3329fe5c
khalied75 <gg35khalid@gmail.com>
2026-04-02 19:15:30 +0300

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <h1 className="text-3xl text-red-500 font-bold underline">Hello, React!</h1>
   </StrictMode>,
 )
